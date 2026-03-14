f0db8cd95820c010f61946921930d32a535344f3
Marcio <marciorefundini@gmail.com>
2026-03-13 22:10:15 -0300

    feat: melhoria no admin mostrando total das vendas fiado

--- mercearia/clientes/admin.py
+++ mercearia/clientes/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Cliente, Produto, Fiado
 
-admin.site.register(Cliente)
-admin.site.register(Produto)
-admin.site.register(Fiado)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nome", "telefone", "endereco")
+
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("nome", "preco")
+
+
+@admin.register(Fiado)
+class FiadoAdmin(admin.ModelAdmin):
+    list_display = ("cliente", "produto", "quantidade", "data", "total")
